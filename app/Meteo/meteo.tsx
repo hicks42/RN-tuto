6b4058e80cf6a28c2c634a0efd888282ba92f13f
hicks42 <gerin.patrice@yahoo.fr>
2024-12-18 10:53:45 +0100

    eas transition

--- app/Meteo/meteo.tsx
+++ app/Meteo/meteo.tsx
@@ -1,7 +1,11 @@
 import AlataRegular from "@/assets/fonts/Alata-Regular.ttf";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Constants from "expo-constants";
+import * as Device from "expo-device";
 import { useFonts } from "expo-font";
-import { StyleSheet, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+import { Platform, StyleSheet, View } from "react-native";
 import Forecast from "./pages/Forecast";
 import Home from "./pages/Home";
 
@@ -11,6 +15,61 @@ const Stack = createNativeStackNavigator();
 
 export default function meteo() {
   const [isFontLoaded] = useFonts({ "Alata-Regular": AlataRegular });
+
+  async function subscribeToNotification() {
+    let token;
+
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("myNotificationChannel", {
+        name: "A channel is needed for the permissions prompt to appear",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      // Learn more about projectId:
+      // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+      // EAS projectId is used here.
+      try {
+        const projectId =
+          Constants?.expoConfig?.extra?.eas?.projectId ??
+          Constants?.easConfig?.projectId;
+        if (!projectId) {
+          throw new Error("Project ID not found");
+        }
+        token = (
+          await Notifications.getExpoPushTokenAsync({
+            projectId,
+          })
+        ).data;
+        console.log(token);
+      } catch (e) {
+        token = `${e}`;
+      }
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    return token;
+  }
+
+  useEffect(() => {
+    subscribeToNotification();
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
       {isFontLoaded ? (
